84db115d6aaac8a93e7110f4409e06ff83b4b294
Antério Vieira <anteriovieira@gmail.com>
2017-03-10 17:16:14 -0300

    Updated

--- src/providers/notification.provider.js
+++ src/providers/notification.provider.js
@@ -1,173 +1,33 @@
-import angular from 'angular'
-
-function NotificationProvider () {
-
-    // Defaults config
-    this.defaultConfig = {}
-
-    // Requite keys
-    this.requiredKeys = []
-
-    /**
-     * Sanitize configuration parameters
-     * 
-     * @param {object} an `object` of params to sanitize
-     * @return {object} an sanitize version of the params
-     */
-    const sanitizeConfigParams = (params) => {
-        if (!(params instanceof Object)) {
-            throw new TypeError('Invalid argument: `config` must be an `Object`.')
-        }
-
-        // Extend default configuration.
-        const config = angular.extend({}, this.defaultConfig, params)
-
-        // Check if all required keys are set.
-        this.requiredKeys.forEach((key) => {
-            if (!config[key]) {
-                throw new Error(`Missing parameter: ${key}.`)
-            }
-        })
-
-        return config
-    }
-
-    /**
-     * Configure
-     * 
-     * @param {object} params - An `object` of params to extend.
-     */
-    this.configure = (params) => {
-        this.defaultConfig = sanitizeConfigParams(params)
-    }
-
-    /**
-     * The provider
-     */
-    this.$get = function($window, $rootScope, $q){
-
-        class Notification 
-        {
-            constructor(config)
-            {   
-                this.config = config
-
-                this.isSupported = !!$window.Notification
-            }
-
-            show(title, options){
-
-                if (!$window.Notification) return false;
-
-                this.title = title;
-                this.options = options;
-
-                // Events cache.
-                this._events = [];
-
-                if ($window.Notification.permission === 'granted') {
-                    return this.createNotification();
-                } else if ($window.Notification.permission !== 'denied') {
-                    this.requestPermission().then(createNotification);
-                }
-            }
-
-            /**
-             * Create the notification.
-             */
-            createNotification()
-            {
-                // Extend options with default provider options.
-                this.options = angular.extend({
-                    focusWindowOnClick: true
-                }, this.config || {}, this.options);
-
-                // Create a base notification.
-                try {
-                    this.baseNotification = new $window.Notification(this.title, this.options);
-                } catch (error) {
-                    return;
-                }
-
-                // Close notification after specified delay.
-                if (this.options.delay) setTimeout(angular.bind(this, this.close), this.options.delay);
-
-                // Focus window on click.
-                if (this.options.focusWindowOnClick) this.$on('click', e => $window.focus() )
-
-                // Re-bind events.
-                this._events.forEach((args) => {
-                    this.$on(...args);
-                });
-
-                // Reset events.
-                this._events = [];
-            }
-
-            requestPermission()
-            {
-                return $q(function (resolve, reject) 
-                {
-                    if (!$window.Notification) return reject();
-
-                    $window.Notification.requestPermission(function (permission) {
-                        // Persist permission.
-                        $window.Notification.permission = $window.Notification.permission || permission;
-                        resolve($window.Notification.permission);
-                    });
-                });
-            }
-
-            /**
-             * Close the notification.
-             */
-            close()
-            {
-                if (this.baseNotification) this.baseNotification.close();
-            }
-
-            /**
-             * Listen on event of a given type.
-             * Supported events are:
-             * 
-             * - error
-             * - show
-             * - click
-             * - close
-             *
-             * @param {String} name
-             * @param {Function} listener
-             */
-            $on(name, listener)
-            {
-                // If the notification is not ready, we cache the event.
-                if (!this.baseNotification) return this._events.push([name, listener]);
-
-                this.baseNotification.addEventListener(name, applyListener);
-
-                function applyListener(...args) {
-                    $rootScope.$apply(function () {
-                        listener(...args);
-                    });
-                }
-
-                // Return the deregistration function.
-                return function $off() {
-                    this.baseNotification.removeListener(event, applyListener);
-                };
-            }
-
-        }
-
-        return new Notification(this.defaultConfig)
-
-    }
-
-    this.$get.$inject = ['$window', '$rootScope', '$q']
+{
+  "name": "ng-desktop-notifications",
+  "version": "0.0.7",
+  "description": "Display desktop notifications to the user.",
+  "main": "./src/ng-desktop-notifications.js",
+  "scripts": {
+    "dev": "rollup -c --watch",
+    "build": "rollup -c",
+    "prepublish": "npm run build",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [
+    "notification",
+    "angular",
+    "desktop"
+  ],
+  "author": "Antério Vieira <anteriovieira@gmail.com>",
+  "license": "MIT",
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/uni-projecao/ng-desktop-notifications.git"
+  },
+  "dependencies": {
+    "angular": "1.6.3"
+  },
+  "devDependencies": {
+    "babel-preset-es2015-rollup": "^3.0.0",
+    "rollup": "^0.41.4",
+    "rollup-plugin-babel": "^2.7.1",
+    "rollup-plugin-uglify": "^1.0.1",
+    "rollup-watch": "^3.2.2"
+  }
 }
-
-
-/**
- * Export NotificationProvider
- */
-export default NotificationProvider
